2f0ba29fe5d7a71a82cfb0e123bbd2e5d353760a
Lualt <58912038+LualtOfficial@users.noreply.github.com>
2023-04-26 11:06:57 +0200

    Initial commit

---
+++ deobf.cs
@@ -0,0 +1,63 @@
+// all the obfuscated class names i know, in no particular order
+
+// also, some of these are accurate/original names and some are completely made up
+// based on what they do and what objects theyre attached to.
+
+global using GameSettings = MonoBehaviourPublicObjomaOblogaTMObseprUnique;
+global using SteamManager = MonoBehaviourPublicObInUIgaStCSBoStcuCSUnique;
+global using GameManager = MonoBehaviourPublicDi2UIObacspDi2UIObUnique;
+global using GameUI = MonoBehaviourPublicGaroloGaObInCacachGaUnique;
+global using ChatBox = MonoBehaviourPublicRaovTMinTemeColoonCoUnique;
+global using DamageVignette = MonoBehaviourPublicRadaSiSiSiUnique;
+global using PlayerMovement = MonoBehaviourPublicGaplfoGaTrorplTrRiBoUnique;
+global using ServerSend = MonoBehaviourPublicInInUnique;
+global using MapManager = MonoBehaviourPublicObInMamaLi1plMadeMaUnique;
+global using GameModeManager = MonoBehaviourPublicGadealGaLi1pralObInUnique;
+global using MapSize = Map.EnumNPublicSealedvasmmelaan5vUnique;
+global using BouncePlayer = MonoBehaviourPublicSicofoSimuupInSiboVeUnique;
+global using JumpPad = MonoBehaviourPublicSiBopuSiUnique;
+global using SharedObjectManager = MonoBehaviourPublicDi2InObInObInUnique;
+global using SnowballPile = MonoBehaviour1PublicBoInSiUnique;
+global using LobbyReadyInteract = MonoBehaviour1PublicTrbuObreunObBoVeVeVeUnique;
+global using GameModeTag = GameModePublicLi1UIUnique;
+global using Packet = ObjectPublicIDisposableLi1ByInByBoUnique;
+global using GameServer = MonoBehaviourPublicObInCoIE85SiAwVoFoCoUnique;
+global using TimerUI = MonoBehaviourPublicTetifrTeStBoStfoSiTiUnique;
+global using Crosshair = MonoBehaviourPublicRedoleReritoboReBoenUnique;
+global using LobbyManager = MonoBehaviourPublicCSDi2UIInstObUIloDiUnique;
+global using ThrownSnowball = MonoBehaviour1PublicTrtrGahiRiCoBoItVeBoUnique;
+global using PlayerInventory = MonoBehaviourPublicItDi2ObIninInTrweGaUnique;
+global using ItemGun = MonoBehaviour2PublicGathObauTrgumuGaSiBoUnique;
+global using NetStatus = MonoBehaviourPublicStLi1InInUnique;
+global using GameLoop = MonoBehaviourPublicObInLi1GagasmLi1GaUnique;
+global using LocalSfx = MonoBehaviourPublicAuhibuAusoObInAuUnique;
+global using VoiceChat = MonoBehaviourPublicAusoMeInObInInInInUnique;
+global using PersistentPlayerData = MonoBehaviourPublicBofrhnBoObInUnique;
+global using ServerClock = MonoBehaviourPublicSiObSiInSiUnique;
+global using EffectManager = MonoBehaviourPublicGataInefObInUnique;
+global using MakeDissonance = MonoBehaviourPublicGadiUnique;
+global using MusicController = MonoBehaviourPublicAuInMeAufuscwiAuObSiUnique;
+global using SongType = MonoBehaviourPublicAuInMeAufuscwiAuObSiUnique.EnumNPublicSealedvaNoInMeFuScWi7vUnique;
+global using Cosmetics = MonoBehaviourPublicLi1CoalDi2InitCoUIUnique;
+global using Quests = MonoBehaviourPublicLi1QudaDi2InquQuacUnique;
+global using SteamInventory = MonoBehaviourPublicStCaSt1ObSthaUIStmaUnique;
+global using InputManager = MonoBehaviourPublicInfobaInlerijuIncrspUnique;
+global using PPController = MonoBehaviourPublicMoBlAmChPoObInUnique;
+global using SteamPacketManager = MonoBehaviourPublicInStInpabyDiInpaby2Unique;
+global using WorkshopUtility = MonoBehaviourPublicStwodeStUnique;
+global using ChatFilter = MonoBehaviourPublicTeprLi1StUnique;
+global using LoadingScreen = MonoBehaviourPublicTeprUIObUIBotiRabamaUnique;
+global using RedLightSafeZone = MonoBehaviourPublicLi1ObsaInObUnique;// rlgl safe zonees are also used in race and king of the hill because of course they are
+global using Ladder = MonoBehaviourPublicLi1CoonUnique;
+global using MovingObject = MonoBehaviourPublicVeofSispRiVeSiofUnique;
+global using WaterSplash = MonoBehaviourPublicGaspLi1ObUnique;
+global using MoveLava = MonoBehaviourPublicVeSioflaSiAulasiBoSiUnique;
+global using KillPlayerOutOfBounds = MonoBehaviourPublicSikiUnique;
+global using GameModeTimer = MonoBehaviourPublicTetifrTeStBoStfoSiTiUnique;
+global using PlayerManager = MonoBehaviourPublicCSstReshTrheObplBojuUnique;
+global using PlayerServerCommunication = MonoBehaviourPublicTrrocaTrInSiVeSipoObUnique;
+global using DetectItems = MonoBehaviourPublicLawhTrcaGacuMaouUnique;
+global using PlayerStatus = MonoBehaviourPublicObcumaObInplInObUnique;
+global using PunchPlayers = MonoBehaviourPublicObsfBoLawhSiUnique;
+global using RevealPlayerNames = MonoBehaviourPublicLi1ObplLawhRaUnique;
+global using MoveCamera = MonoBehaviourPublicTrplVeofdeVevaCaRiVeUnique;
